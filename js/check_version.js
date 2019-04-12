d2eb07c803e7a08bb489c487df3292458ec730b9
wuqibo <wqb55@qq.com>
2019-04-12 20:50:47 +0800

    none

--- js/check_version.js
+++ js/check_version.js
@@ -2,19 +2,19 @@
 //{"version":"1.0.0","url":"http://120.79.197.189:3051/shentongfuzhu_v2.apk"}
 
 const check = function(jsonUrl) {
-	// #ifdef APP-PLUS
 	uni.request({
 		url: jsonUrl + '?random=' + Math.random(),
 		success: (res) => {
 			console.log(JSON.stringify(res));
-			let versionName = res.data.version;
-			let versionUrl = res.data.url;
-			console.log('versionName:' + versionName);
-			console.log('versionUrl:' + versionUrl);
-			if (plus.runtime.version != versionName) {
+			let version = res.data.version;
+			let url = res.data.url;
+			console.log('version:' + version);
+			console.log('url:' + url);
+			// #ifdef APP-PLUS
+			if (plus.runtime.version != version) {
 				uni.showModal({
 					title: '新版本已发布，是否立即更新',
-					content: (uni.getSystemInfoSync().platform == 'ios') ? '':'(更新将在后台执行,可下拉状态栏查看)',
+					content: (uni.getSystemInfoSync().platform == 'android') ? '(更新将在后台执行,可下拉状态栏查看)':'',
 					showCancel: true,
 					cancelText: '暂不更新',
 					cancelColor: '#666666',
@@ -22,19 +22,19 @@ const check = function(jsonUrl) {
 					success: res => {
 						if (res.confirm) {
 							console.log('用户点击确定');
-							plus.runtime.openURL(versionUrl);
+							plus.runtime.openURL(url);
 						} else if (res.cancel) {
 							console.log('用户点击取消');
 						}
 					}
 				});
 			}
+			// #endif
 		},
 		fail: (err) => {
 			console.error(JSON.stringify(err));
 		}
 	});
-	// #endif
 }
 
 export default {
