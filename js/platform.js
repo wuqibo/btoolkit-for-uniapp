5f2d5247d862d711267f06b52c11f3f35417797b
wuqibo <wqb55@qq.com>
2019-04-18 12:16:32 +0800

    no description

--- js/platform.js
+++ js/platform.js
@@ -1,4 +1,4 @@
-//安卓返回：android  苹果返回：ios
+//安卓返回：android  苹果返回：ios  微信开发者工具：devtools
 const getPlatformName = function() {
 	return uni.getSystemInfoSync().platform;
 }
@@ -13,8 +13,14 @@ const isIOS = function() {
 	return getPlatformName == 'ios';
 }
 
+//是否是iOS端
+const isWeiXinDevTools = function() {
+	return getPlatformName == 'devtools';
+}
+
 export default {
 	getPlatformName,
 	isAndroid,
-	isIOS
+	isIOS,
+	isWeiXinDevTools
 }
